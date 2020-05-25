01315f7178b08ae994d39051d848dca96db99cb1
Verayannti Siregar <65701771+verayantisiregar@users.noreply.github.com>
2020-05-25 19:03:26 +0700

    Adding Individual Task

---
+++ Tugas Kalkulator/WindowsFormsApplication8/WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_satu = new System.Windows.Forms.TextBox();
+            this.txt_dua = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_tiga = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_hasil = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "+";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "angka 1";
+            // 
+            // txt_satu
+            // 
+            this.txt_satu.Location = new System.Drawing.Point(120, 77);
+            this.txt_satu.Name = "txt_satu";
+            this.txt_satu.Size = new System.Drawing.Size(100, 26);
+            this.txt_satu.TabIndex = 2;
+            // 
+            // txt_dua
+            // 
+            this.txt_dua.Location = new System.Drawing.Point(120, 121);
+            this.txt_dua.Name = "txt_dua";
+            this.txt_dua.Size = new System.Drawing.Size(100, 26);
+            this.txt_dua.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "angka 2";
+            // 
+            // txt_tiga
+            // 
+            this.txt_tiga.Location = new System.Drawing.Point(120, 163);
+            this.txt_tiga.Name = "txt_tiga";
+            this.txt_tiga.Size = new System.Drawing.Size(100, 26);
+            this.txt_tiga.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "angka 3";
+            // 
+            // txt_hasil
+            // 
+            this.txt_hasil.Location = new System.Drawing.Point(396, 77);
+            this.txt_hasil.Name = "txt_hasil";
+            this.txt_hasil.Size = new System.Drawing.Size(100, 26);
+            this.txt_hasil.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(315, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "hasil";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 30);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "kubik";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 269);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(15, 311);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 30);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "x";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(15, 357);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 30);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "/";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(120, 233);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 30);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "%";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(120, 271);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 30);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "pangkat";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(201, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(235, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "*angka 2 dan angka 3 dikosongi";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(201, 311);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(235, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "*angka 2 dan angka 3 dikosongi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(297, 29);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "OPERASI MATEMATIKA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(580, 420);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txt_hasil);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_tiga);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_dua);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_satu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_satu;
+        private System.Windows.Forms.TextBox txt_dua;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_tiga;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_hasil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Tugas Kalkulator/WindowsFormsApplication8/WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Operasi tmbh = new Operasi();
+            tmbh.x = Convert.ToDouble(txt_satu.Text);
+            tmbh.y = Convert.ToDouble(txt_dua.Text);
+            tmbh.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(tmbh.tambah());
+           
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Operasi krg = new Operasi();
+            krg.x = Convert.ToDouble(txt_satu.Text);
+            krg.y = Convert.ToDouble(txt_dua.Text);
+            krg.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(krg.kurang());
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Operasi kali = new Operasi();
+            kali.x = Convert.ToDouble(txt_satu.Text);
+            kali.y = Convert.ToDouble(txt_dua.Text);
+            kali.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(kali.kali());
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Operasi bg = new Operasi();
+            bg.x = Convert.ToDouble(txt_satu.Text);
+            bg.y = Convert.ToDouble(txt_dua.Text);
+            bg.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(bg.bagi());
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Operasi mdls = new Operasi();
+            mdls.x = Convert.ToDouble(txt_satu.Text);
+            mdls.y = Convert.ToDouble(txt_dua.Text);
+            mdls.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(mdls.modulus());
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Operasi pkt = new Operasi();
+            pkt.x = Convert.ToDouble(txt_satu.Text);
+            pkt.y = Convert.ToDouble(txt_dua.Text);
+            pkt.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(pkt.pkt());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Operasi kbk = new Operasi();
+            kbk.x = Convert.ToDouble(txt_satu.Text);
+            kbk.y = Convert.ToDouble(txt_dua.Text);
+            kbk.z = Convert.ToDouble(txt_tiga.Text);
+
+            txt_hasil.Text = Convert.ToString(kbk.kbk());
+
+        }
+    }
+}
